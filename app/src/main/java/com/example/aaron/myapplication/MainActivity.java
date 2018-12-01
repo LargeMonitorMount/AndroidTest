4eda69c3f6650e730378c32167c1c85e28e69b44
recskabajnok <recskabajnok@gmail.com>
2018-12-01 19:08:21 +0100

    2

--- app/src/main/java/com/example/aaron/myapplication/MainActivity.java
+++ app/src/main/java/com/example/aaron/myapplication/MainActivity.java
@@ -11,11 +11,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private TextView homeTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -24,18 +24,30 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    homeTextMessage.setText(R.string.title_home);
+                    removeAllelements();
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    homeTextMessage.setText(R.string.title_dashboard);
+                    removeAllelements();
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    homeTextMessage.setText(R.string.title_notifications);
+                    removeAllelements();
                     return true;
             }
             return false;
         }
     };
+
+    private void removeAllelements() {
+        for (View v: GUIelements
+             ) {
+            v.setVisibility(View.GONE);
+        }
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,39 +56,55 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (!RootUtil.isDeviceRooted()) {
-            //ShowphoneNotRootedDialogAndExit();
-        }else {setupGUIandListeners();}
+
+
+        //if (!RootUtil.isDeviceRooted()) {
+        if (false){
+
+            ShowphoneNotRootedDialogAndExit();
+
+
+        }else {
+
+            setupGUIandListeners();
+        }
 
 
 
     }
-
+    ArrayList<View> GUIelements = new ArrayList<>();
+    TextView hParams;
+    TextView hTextView;
     private void setupGUIandListeners() {
-        final TextView textView;
+
         Button btn;
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        homeTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        textView = (TextView)findViewById(R.id.textView);
+        hTextView = (TextView)findViewById(R.id.htextView);
         btn = (Button) findViewById(R.id.btn);
-        final TextView params = (TextView) findViewById(R.id.params);
+        hParams = (TextView) findViewById(R.id.hParams);
 
-
+        GUIelements.add(homeTextMessage);
+        GUIelements.add(hTextView);
+        GUIelements.add(hParams);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // v.setBackgroundColor(65);
-                textView.append(params.getText());
-                triggerStartEvent(findViewById(R.id.params).toString().split(" "));
+                hTextView.append(hParams.getText());
+                triggerStartEvent(findViewById(R.id.hParams).toString().split(" "));
 
             }
 
             private void triggerStartEvent(String[] args) {
 
-              }
+
+
+
+            }
 
 
         });
@@ -93,6 +121,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int which) {
                 finish();
                 System.exit(0);
+
+
+
             }
         });
 
